6f487615fc5cd0c362e7520995404582676e9276
Noa Dudai <nooninana@gmail.com>
2025-01-12 00:46:17 +0200

    att1 workflow

--- UserShiftsApiService/UserShiftsApiService/Program.cs
+++ UserShiftsApiService/UserShiftsApiService/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -10,6 +11,25 @@ builder.Services.AddControllers();
 
 builder.Services.AddOpenApi();
 
+var configuration = builder.Configuration;
+
+if (Assembly.GetEntryAssembly()?.GetName().Name != "GetDocument.Insider")
+{
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowFront", policy =>
+        {
+            policy.WithOrigins(configuration["FrontUrl"]).AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials().
+                AllowCredentials();
+        });
+    });
+}
+
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -19,9 +39,15 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+if (Assembly.GetEntryAssembly()?.GetName().Name != "GetDocument.Insider")
+{
+    app.UseCors("AllowFront");
+}
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.MapOpenApi();
 
 app.Run();
