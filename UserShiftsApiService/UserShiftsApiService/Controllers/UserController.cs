26b3f18dd073e5427648c2e4ef8a6260bbda0d39
Noa Dudai <nooninana@gmail.com>
2025-01-15 14:04:21 +0200

    get user info

---
+++ UserShiftsApiService/UserShiftsApiService/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UserShiftsApiService.Models;
+
+namespace UserShiftsApiService.Controllers;
+
+[ApiController]
+[Route("/users/")]
+public class UserController : ControllerBase
+{
+    private readonly ShiftsSchedulingContext _dbContext;
+
+    public UserController(ShiftsSchedulingContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    [HttpGet]
+    [Authorize]
+    [Route("get-user")]
+    public async Task<IActionResult> GetUser()
+    {
+        var authSub = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var user = await _dbContext.Users.SingleAsync(u => u.AuthSub == authSub);
+        
+        return Ok(user.Id);
+    }
+}
