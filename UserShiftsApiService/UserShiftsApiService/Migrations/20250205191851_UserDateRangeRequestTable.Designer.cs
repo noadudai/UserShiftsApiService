6f01d927eef23911bf25f5f774064847b7f65c96
Noa Dudai <nooninana@gmail.com>
2025-02-05 21:20:02 +0200

    new migrations

--- UserShiftsApiService/UserShiftsApiService/Migrations/20250205191851_UserDateRangeRequestTable.Designer.cs
+++ UserShiftsApiService/UserShiftsApiService/Migrations/20250205191851_UserDateRangeRequestTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UserShiftsApiService.Models;
@@ -11,9 +12,11 @@ using UserShiftsApiService.Models;
 namespace UserShiftsApiService.Migrations
 {
     [DbContext(typeof(ShiftsSchedulingContext))]
-    partial class ShiftsSchedulingContextModelSnapshot : ModelSnapshot
+    [Migration("20250205191851_UserDateRangeRequestTable")]
+    partial class UserDateRangeRequestTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UserShiftsApiService/UserShiftsApiService/Migrations/20250205191851_UserDateRangeRequestTable.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserShiftsApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserDateRangeRequestTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UserDateRangeScheduleRequests",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    StartingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    EndingDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    RequestType = table.Column<int>(type: "integer", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDateRangeScheduleRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserDateRangeScheduleRequests_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserDateRangeScheduleRequests_UserId",
+                table: "UserDateRangeScheduleRequests",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserDateRangeScheduleRequests");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
