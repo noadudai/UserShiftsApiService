cccb519ea3eb13a3c9ec34dda3ab88b2a01c9108
Noa Dudai <nooninana@gmail.com>
2025-04-21 12:43:48 +0300

    string enum

--- UserShiftsApiService/UserShiftsApiService/Entities/UserDateRangeScheduleRequestEntityConfiguration.cs
+++ UserShiftsApiService/UserShiftsApiService/Entities/UserDateRangeScheduleRequestEntityConfiguration.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace UserShiftsApiService.Entities;
 
@@ -11,11 +12,14 @@ public class UserDateRangeScheduleRequestEntityTypeConfiguration : IEntityTypeCo
         builder.Property(p => p.Id).IsRequired();
         builder.Property(p => p.StartingDate).IsRequired();
         builder.Property(p => p.EndingDate).IsRequired();
-        builder.Property(p => p.RequestType).IsRequired();
         builder.Property(p => p.UserId).IsRequired();
         
         builder.HasOne(dateRangePrefs => dateRangePrefs.User)
             .WithMany(u => u.DateRangePreferences)
             .HasForeignKey(dateRangePrefs => dateRangePrefs.UserId);
+        
+        builder.Property(p => p.RequestType)
+            .HasConversion(new EnumToStringConverter<ShiftRequestType>())
+            .IsRequired();
     }
 }
