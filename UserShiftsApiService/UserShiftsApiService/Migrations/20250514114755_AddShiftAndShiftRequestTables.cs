ecd699c3e71a895847e3d2164eea1359a9874c55
Noa Dudai <nooninana@gmail.com>
2025-05-14 14:49:58 +0300

    only one relevant migration

--- UserShiftsApiService/UserShiftsApiService/Migrations/20250514114755_AddShiftAndShiftRequestTables.cs
+++ UserShiftsApiService/UserShiftsApiService/Migrations/20250514114755_AddShiftAndShiftRequestTables.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -7,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace UserShiftsApiService.Migrations
 {
     /// <inheritdoc />
-    public partial class ShiftEntityAndShiftsRequestEntity : Migration
+    public partial class AddShiftAndShiftRequestTables : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -16,7 +15,7 @@ namespace UserShiftsApiService.Migrations
                 name: "Shifts",
                 columns: table => new
                 {
-                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<string>(type: "text", nullable: false),
                     ShiftType = table.Column<string>(type: "text", nullable: false),
                     StartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
                     EndDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
@@ -31,9 +30,8 @@ namespace UserShiftsApiService.Migrations
                 columns: table => new
                 {
                     Id = table.Column<string>(type: "text", nullable: false),
-                    ShiftRequestType = table.Column<int>(type: "integer", nullable: false),
-                    ShiftIds = table.Column<List<Guid>>(type: "uuid[]", nullable: false),
-                    UserId = table.Column<string>(type: "text", nullable: false)
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ShiftRequestType = table.Column<string>(type: "text", nullable: false)
                 },
                 constraints: table =>
                 {
@@ -46,6 +44,41 @@ namespace UserShiftsApiService.Migrations
                         onDelete: ReferentialAction.Cascade);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "RequestedShift",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    UserShiftsRequestId = table.Column<string>(type: "text", nullable: false),
+                    ShiftId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RequestedShift", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RequestedShift_Shifts_ShiftId",
+                        column: x => x.ShiftId,
+                        principalTable: "Shifts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RequestedShift_UserShiftPreferenceRequests_UserShiftsReques~",
+                        column: x => x.UserShiftsRequestId,
+                        principalTable: "UserShiftPreferenceRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestedShift_ShiftId",
+                table: "RequestedShift",
+                column: "ShiftId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RequestedShift_UserShiftsRequestId",
+                table: "RequestedShift",
+                column: "UserShiftsRequestId");
+
             migrationBuilder.CreateIndex(
                 name: "IX_UserShiftPreferenceRequests_UserId",
                 table: "UserShiftPreferenceRequests",
@@ -55,6 +88,9 @@ namespace UserShiftsApiService.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "RequestedShift");
+
             migrationBuilder.DropTable(
                 name: "Shifts");
 
