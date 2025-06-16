adbb692863c42fca84cd6d694d59c0842cff7092
Noa Dudai <nooninana@gmail.com>
2025-06-16 22:06:40 +0300

    rename

--- UserShiftsApiService/UserShiftsApiService/Models/ShiftsSchedulingContext.cs
+++ UserShiftsApiService/UserShiftsApiService/Models/ShiftsSchedulingContext.cs
@@ -8,7 +8,7 @@ public class ShiftsSchedulingContext : DbContext
     public DbSet<UserEntity> Users { get; set; }
     public DbSet<UserDateRangePreferenceRequestEntity> UserDateRangeScheduleRequests { get; set; }
     public DbSet<UserShiftsPreferenceRequestEntity> UserShiftPreferenceRequests { get; set; }
-    public DbSet<RequestedShiftEntity> RequestedShift { get; set; }
+    public DbSet<RequestedShiftEntity> ShiftRequests { get; set; }
     public DbSet<ShiftEntity> Shifts { get; set; }
 
     public ShiftsSchedulingContext(DbContextOptions<ShiftsSchedulingContext> options)
