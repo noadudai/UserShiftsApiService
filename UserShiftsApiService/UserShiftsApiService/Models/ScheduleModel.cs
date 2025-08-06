15b15d31430efaf84342c3b5d16ef995b255d539
Noa Dudai <nooninana@gmail.com>
2025-08-06 16:21:58 +0300

    class not record

--- UserShiftsApiService/UserShiftsApiService/Models/ScheduleModel.cs
+++ UserShiftsApiService/UserShiftsApiService/Models/ScheduleModel.cs
@@ -3,7 +3,7 @@ using UserShiftsApiService.Entities;
 
 namespace UserShiftsApiService.Models;
 
-public record ScheduleModel
+public class ScheduleModel
 {
     public required ScheduleEntity Schedule {get; set;}
     public List<ShiftEntity> Shifts {get; set;}
